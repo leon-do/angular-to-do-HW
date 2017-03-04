7a3aa5f4786d5e8bc38f6be16b0de15df20b1107
Leon Do <leondo123@gmail.com>
2017-03-04 00:23:09 -0500

    url

--- app/mongo.js
+++ app/mongo.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var serverJS = require('../server.js')
 
 //https://www.youtube.com/watch?v=ZIu3joJRLzM
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/testdb');
+mongoose.connect('mongodb://heroku_nj250c2q:u75crcbccd9ljaquobt08sg2r7@ds117830.mlab.com:17830/heroku_nj250c2q' || 'mongodb://127.0.0.1:27017/testdb');
 
 //make model
 var angular2Do = mongoose.model("angular2DoCollection", {phrase:String, complete:Boolean})
